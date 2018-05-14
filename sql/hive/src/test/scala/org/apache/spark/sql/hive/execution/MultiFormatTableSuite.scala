8c0e2a01411805155963cf5ef60f35e1bc711d13
Kris Geusebroek <krisgeus@gmail.com>
2018-05-14 07:58:17 +0200

    Add failing test with data in multiple formats

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/MultiFormatTableSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/MultiFormatTableSuite.scala
@@ -17,13 +17,16 @@
 
 package org.apache.spark.sql.hive.execution
 
-import java.io.File
+import java.io.{BufferedInputStream, File, FileInputStream}
+import java.net.URI
 
 import org.scalatest.BeforeAndAfterEach
 
-import org.apache.spark.sql.QueryTest
+import org.apache.spark.sql.{DataFrame, QueryTest, Row}
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.catalog.CatalogTableType
+import org.apache.spark.sql.catalyst.parser
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
 import org.apache.spark.sql.execution.SparkSqlParser
 import org.apache.spark.sql.execution.command.DDLUtils
 import org.apache.spark.sql.hive.test.TestHiveSingleton
@@ -33,6 +36,8 @@ import org.apache.spark.sql.test.SQLTestUtils
 class MultiFormatTableSuite
   extends QueryTest with SQLTestUtils with TestHiveSingleton with BeforeAndAfterEach{
   import testImplicits._
+
+  val parser = new SparkSqlParser(new SQLConf())
 
   override def afterEach(): Unit = {
     try {
@@ -172,86 +177,245 @@ class MultiFormatTableSuite
 //    }
 //  }
 
-  private lazy val parser = new SparkSqlParser(new SQLConf)
+  val partitionCol = "dt"
+  val partitionVal1 = "2018-01-26"
+  val partitionVal2 = "2018-01-27"
+  private case class PartitionDefinition(
+    column: String,
+    value: String,
+    location: Option[URI] = None,
+    format: Option[String] = None
+  ) {
+    def toSpec: String = {
+      s"($column='$value')"
+    }
+    def toSpecAsMap: Map[String, String] = {
+      Map(column -> value)
+    }
+  }
 
-  test("create hive table with parquet data in partitions") {
+  test("create hive table with multi format partitions") {
     val catalog = spark.sessionState.catalog
-    withTempDir { tmpDir =>
-      val basePath = tmpDir.getCanonicalPath
-      val partitionPath_part1 = new File(basePath + "/dt=2018-01-26")
-      val partitionPath_part2 = new File(basePath + "/dt=2018-01-27")
-      val dirSet =
+    withTempDir { baseDir =>
+      val basePath = baseDir.getCanonicalPath
+      val partitionPath_part1 = new File(basePath + s"/$partitionCol=$partitionVal1")
+      val partitionPath_part2 = new File(basePath + s"/$partitionCol=$partitionVal2")
+      val allDirs =
+        baseDir :: partitionPath_part1 :: partitionPath_part2 :: Nil
+      val partitionDirs =
         partitionPath_part1 :: partitionPath_part2 :: Nil
 
       val resultTable = "ext_multiformat_partition_table"
       withTable(resultTable) {
-        assert(tmpDir.listFiles.isEmpty)
-        sql(
-          s"""
-             |CREATE EXTERNAL TABLE $resultTable (key INT, value STRING)
-             |PARTITIONED BY (dt STRING)
-             |STORED AS PARQUET
-             |LOCATION '${tmpDir.toURI}'
-          """.stripMargin)
+        assert(baseDir.listFiles.isEmpty)
 
-        sql(
-          s"""
-             |ALTER TABLE $resultTable ADD
-             |PARTITION (dt='2018-01-26') LOCATION '${partitionPath_part1.toURI}'
-             |PARTITION (dt='2018-01-27') LOCATION '${partitionPath_part2.toURI}'
-          """.stripMargin)
+        createExternalTable(resultTable, baseDir.toURI)
 
-        val sql5 = s"""
-             |ALTER TABLE $resultTable PARTITION (dt='2018-01-26') SET FILEFORMAT PARQUET
-          """.stripMargin
+        // Before partition creation, all the directory are empty
+        assert(allDirs.forall(dir => dir.listFiles == null || dir.listFiles.isEmpty))
 
-        sql(
-          s"""
-             |ALTER TABLE $resultTable PARTITION (dt='2018-01-26') SET FILEFORMAT PARQUET
-          """.stripMargin)
-        sql(
-          s"""
-             |ALTER TABLE $resultTable PARTITION (dt='2018-01-27') SET FILEFORMAT AVRO
-          """.stripMargin)
+        val partitions = List(
+          PartitionDefinition(
+            partitionCol, partitionVal1, Some(partitionPath_part1.toURI), format = Some("PARQUET")
+          ),
+          PartitionDefinition(
+            partitionCol, partitionVal2, Some(partitionPath_part2.toURI), format = Some("AVRO")
+          )
+        )
+        addPartitions(resultTable, partitions)
+        // After partition creation, all the partition directories are empty
+        assert(partitionDirs.forall(dir => dir.listFiles == null || dir.listFiles.isEmpty))
+        // baseDir is not (contains the partition dirs)
+        assert(baseDir.listFiles().nonEmpty)
+        assert(baseDir.listFiles().toSeq == partitionDirs)
 
-        // Before data insertion, all the directory are empty
-//        assert(dirSet.forall(dir => dir.listFiles == null || dir.listFiles.isEmpty))
+        setPartitionFormat(resultTable, partitions.head)
+        setPartitionFormat(resultTable, partitions.last)
 
+        // Check table storage type is PARQUET
         val hiveResultTable =
           catalog.getTableMetadata(TableIdentifier(resultTable, Some("default")))
         assert(DDLUtils.isHiveTable(hiveResultTable))
         assert(hiveResultTable.tableType == CatalogTableType.EXTERNAL)
-        assert(hiveResultTable.storage.inputFormat ==
-          Some("org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"))
-        assert(hiveResultTable.storage.outputFormat ==
-          Some("org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"))
-        assert(hiveResultTable.storage.serde ==
-          Some("org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"))
+        assert(hiveResultTable.storage.inputFormat
+          .contains("org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat")
+        )
+        assert(hiveResultTable.storage.outputFormat
+          .contains("org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat")
+        )
+        assert(hiveResultTable.storage.serde
+          .contains("org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe")
+        )
 
+        // Check table has correct partititons
         assert(
           catalog.listPartitions(TableIdentifier(resultTable,
-            Some("default"))).map(_.spec).toSet ==
-            Set(Map("dt" -> "2018-01-26"), Map("dt"  -> "2018-01-27"))
+            Some("default"))).map(_.spec).toSet == partitions.map(_.toSpecAsMap).toSet
         )
 
+        // Check first table partition storage type is PARQUET
         assert(
           catalog.getPartition(
             TableIdentifier(resultTable, Some("default")),
-            Map("dt" -> "2018-01-26")
-          ).storage.serde == Some("org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe")
+            partitions.head.toSpecAsMap
+          ).storage.serde.contains("org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe")
         )
 
+        // Check second table partition storage type is AVRO
         assert(
           catalog.getPartition(
             TableIdentifier(resultTable, Some("default")),
-            Map("dt" -> "2018-01-27")
-          ).storage.serde == Some("org.apache.hadoop.hive.serde2.avro.AvroSerDe")
+            partitions.last.toSpecAsMap
+          ).storage.serde.contains("org.apache.hadoop.hive.serde2.avro.AvroSerDe")
         )
 
-        sql(s"DROP TABLE $resultTable")
-        // drop table will not delete the data of external table
-//        assert(dirSet.forall(dir => dir.listFiles.nonEmpty))
       }
     }
+  }
+
+  test("create hive table with multi format partitions containing correct data") {
+    val catalog = spark.sessionState.catalog
+    withTempDir { baseDir =>
+      val basePath = baseDir.getCanonicalPath
+      val partitionPath_part1 = new File(basePath + s"/$partitionCol=$partitionVal1")
+      val partitionPath_part2 = new File(basePath + s"/$partitionCol=$partitionVal2")
+      val allDirs =
+        baseDir :: partitionPath_part1 :: partitionPath_part2 :: Nil
+      val partitionDirs =
+        partitionPath_part1 :: partitionPath_part2 :: Nil
+
+      val resultTable = "ext_multiformat_partition_table_with_data"
+      val avroPartitionTable = "ext_avro_partition_table"
+      withTable(resultTable, avroPartitionTable) {
+        assert(baseDir.listFiles.isEmpty)
+
+        createExternalTable(resultTable, baseDir.toURI)
+
+        // Before partition creation, all the directory are empty
+        assert(allDirs.forall(dir => dir.listFiles == null || dir.listFiles.isEmpty))
+
+        val partitions = List(
+          PartitionDefinition(
+            partitionCol, partitionVal1, Some(partitionPath_part1.toURI), format = Some("PARQUET")
+          ),
+          PartitionDefinition(
+            partitionCol, partitionVal2, Some(partitionPath_part2.toURI), format = Some("AVRO")
+          )
+        )
+
+        addPartitions(resultTable, partitions)
+        // After partition creation, all the partition directories are empty
+        assert(partitionDirs.forall(dir => dir.listFiles == null || dir.listFiles.isEmpty))
+        // baseDir is not (contains the partition dirs)
+        assert(baseDir.listFiles().nonEmpty)
+        assert(baseDir.listFiles().toSeq == partitionDirs)
+
+        setPartitionFormat(resultTable, partitions.head)
+        setPartitionFormat(resultTable, partitions.last)
+
+        // INSERT OVERWRITE TABLE only works for the default table format.
+        // So we can use it here to insert data into the parquet partition
+        sql(
+          s"""
+             |INSERT OVERWRITE TABLE $resultTable
+             |PARTITION ${partitions.head.toSpec}
+             |SELECT 1 as id, 'a' as value
+                  """.stripMargin)
+
+        val parquetData = spark.read.parquet(partitionPath_part1.getCanonicalPath)
+        checkAnswer(parquetData, Row(1, "a"))
+
+        // The only valid way to insert avro data into the avro partition
+        // is to create a new avro table directly on the location of the avro partition
+        val avroSchema =
+          """{
+            |  "name": "baseRecord",
+            |  "type": "record",
+            |  "fields": [{
+            |    "name": "key",
+            |    "type": ["null", "int"],
+            |    "default": null
+            |  },
+            |  {
+            |    "name": "value",
+            |    "type": ["null", "string"],
+            |    "default": null
+            |  }]
+            |}
+          """.stripMargin
+
+        // Creates the Avro table
+        sql(
+          s"""
+             |CREATE TABLE $avroPartitionTable
+             |ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
+             |STORED AS
+             |  INPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'
+             |  OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'
+             |LOCATION '${partitionPath_part2.getCanonicalPath}'
+             |TBLPROPERTIES ('avro.schema.literal' = '$avroSchema')
+            """.stripMargin
+        )
+
+        sql(
+          s"""
+             |INSERT OVERWRITE TABLE $avroPartitionTable
+             |SELECT 2, 'b'
+           """.stripMargin
+        )
+
+        // Directly reading from the avro table should yield correct results
+        val avroData = spark.read.table(avroPartitionTable)
+        checkAnswer(avroData, Row(2, "b"))
+
+        // Selecting data from the partition currently fails because it tries to
+        // read avro data with parquet reader
+        val avroPartitionData = sql(
+          s"""
+             |SELECT key, value FROM ${resultTable}
+             |WHERE ${partitionCol}='${partitionVal2}'
+           """.stripMargin
+        )
+        checkAnswer(avroPartitionData, Row(2, "b"))
+
+      }
+    }
+  }
+
+  private def createExternalTable(table: String, location: URI): DataFrame = {
+    sql(
+      s"""
+         |CREATE EXTERNAL TABLE $table (key INT, value STRING)
+         |PARTITIONED BY (dt STRING)
+         |STORED AS PARQUET
+         |LOCATION '$location'
+      """.stripMargin
+    )
+  }
+
+  private def addPartitions(table: String, partitionDefs: List[PartitionDefinition]): DataFrame = {
+    val partitions = partitionDefs
+      .map(definition => s"PARTITION ${definition.toSpec} LOCATION '${definition.location.get}'")
+      .mkString("\n")
+
+    sql(
+      s"""
+         |ALTER TABLE $table ADD
+         |$partitions
+      """.stripMargin
+    )
+
+  }
+
+  private def setPartitionFormat(
+                                  table: String,
+                                  partitionDef: PartitionDefinition
+                                ): DataFrame = {
+    sql(
+      s"""
+         |ALTER TABLE $table
+         |PARTITION ${partitionDef.toSpec} SET FILEFORMAT ${partitionDef.format.get}
+      """.stripMargin
+    )
   }
 }
